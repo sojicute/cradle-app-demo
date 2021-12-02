815bee02a05d9f6a5abb6ea0e709caee2d9d902e
sojicute <sojicute137@gmail.com>
2021-12-02 15:28:32 +0400

    Created new project "cradle"

---
+++ src/main/java/com/sojicute/cradle/CradleApplication.java
@@ -0,0 +1,13 @@
+package com.sojicute.cradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CradleApplication.class, args);
+    }
+
+}
