44eec09f5b0ed533d1aef415e6af4485f023ca48
sojicute <sojicute137@gmail.com>
2021-12-13 17:40:50 +0400

    deleted OwnerService

--- src/main/java/com/sojicute/cradle/repository/RoadRepository.java
+++ src/main/java/com/sojicute/cradle/repository/RoadRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface RoadRepository extends JpaRepository<Road, Long> {
-    List<Road> findRoadsByUser_Username(String username);
 }
--- src/test/java/com/sojicute/cradle/services/RoadServiceImplTest.java
+++ src/test/java/com/sojicute/cradle/services/RoadServiceImplTest.java
@@ -1,21 +1,41 @@
 package com.sojicute.cradle.services;
 
+import com.sojicute.cradle.domain.Road;
+import com.sojicute.cradle.repository.RoadRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import static org.assertj.core.api.Assertions.assertThat;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class RoadServiceImplTest {
+
+    @Autowired
+    private RoadServiceImpl roadService;
+
+    @MockBean
+    private RoadRepository roadRepository;
 
     @Test
     void findAll() {
     }
 
     @Test
-    void findRoadById() {
+    void shouldFindRoadById() {
+        Road road = new Road();
+        Mockito.when(roadRepository.findById(1L)).thenReturn(Optional.of(road));
+
+        Road exceptRoad = roadService.findRoadById(1L);
+
+        assertThat(exceptRoad).isNotNull();
     }
 
     @Test
     void addNewRoad() {
+
     }
 
     @Test
